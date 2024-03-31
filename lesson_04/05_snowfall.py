f990fe6aec6ad6b8b4da424585d161db51bed3ec
kunai <neni23@mail.ru>
2024-03-31 17:41:10 +0300

    snowfall done, but no cycle. idk now

--- lesson_04/05_snowfall.py
+++ lesson_04/05_snowfall.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 800)
 
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
@@ -17,12 +18,37 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
+x1 = 100
+y1 = 750
+
+x2 = 120
+y2 = 650
+
+x3 = 140
+y3 = 700
+
 while True:
     sd.clear_screen()
-    pass
-    pass
-    pass
+    # point = sd.get_point(x1, y1)
+    # sd.snowflake(center=point, length=30, color=sd.COLOR_WHITE)
+    # y1 -= 30
+    # x1 += 15
+    # if y1 < -10:
+    #     break
+    #
+    # point = sd.get_point(x2, y2)
+    # sd.snowflake(center=point, length=15, color=sd.COLOR_WHITE)
+    # y2 -= 30
+    # x2 += 30
+    # if y2 < -10:
+    #     break
+    #
+    # point = sd.get_point(x3, y3)
+    # sd.snowflake(center=point, length=23, color=sd.COLOR_WHITE)
+    # y3 -= 30
+    # x3 += 10
+    # if y3 < -10:
+    #     break
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
