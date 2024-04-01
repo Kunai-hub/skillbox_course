cca8987b11576f7eee04f733058967e16825ceb1
kunai <neni23@mail.ru>
2024-04-01 18:33:50 +0300

    07 done with remark!

--- lesson_03/07_wall.py
+++ lesson_03/07_wall.py
@@ -6,30 +6,29 @@ import simple_draw as sd
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# for y_1, y_2 in range(1, 1001, 100):
-#     for x_1, x_2 in range(1, 501, 50):
-#         point = sd.get_point(x_1, y)
-#         sd.rectangle(left_bottom=point, right_top=point)
 
-start_x = -50
-end_x = 50
-start_y = 0
-end_y = 50
-step = -500
+# start_x = -50
+# end_x = 50
+# start_y = 0
+# end_y = 50
+# step = -500
+#
+# for y in range(0, 600, 50):
+#     step += 50
+#     for x in range(0, 1500, 100):
+#         point_1 = sd.get_point(start_x + x + step, start_y + y)
+#         point_2 = sd.get_point(end_x + x + step, end_y + y)
+#         sd.rectangle(point_1, point_2, width=1)
 
+
+row = 0
 for y in range(0, 600, 50):
-    step += 50
-    for x in range(0, 1500, 100):
-        point_1 = sd.get_point(start_x + x + step, start_y + y)
-        point_2 = sd.get_point(end_x + x + step, end_y + y)
+    row += 1
+    for x in range(0, 600, 100):
+        x_0 = x if row % 2 else x + 100 // 2
+        point_1 = sd.get_point(x_0, y)
+        point_2 = sd.get_point(x_0 + 100, y + 50)
         sd.rectangle(point_1, point_2, width=1)
 
-
-
-
-
-# point_1 = sd.get_point(-50, 0)
-# point_2 = sd.get_point(50, 50)
-# sd.rectangle(point_1, point_2, width=2)
 
 sd.pause()
