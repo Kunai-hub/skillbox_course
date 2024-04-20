25990749f29c470258f3a66b79adc46cd0fefa1b
kunai <neni23@mail.ru>
2024-04-20 10:47:49 +0300

    first part (w/o class method) 01_char_stat

--- lesson_09/01_char_stat.py
+++ lesson_09/01_char_stat.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import zipfile
+from pprint import pprint
 
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
@@ -21,7 +23,42 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+# file_name = 'C:\\Users\\neni2\PycharmProjects\\Kirill\kunai\lesson_09\\python_snippets\\voyna-i-mir.txt.zip'
+# file_zip = zipfile.ZipFile(file=file_name, mode='r')
+#
+# for file_txt in file_zip.namelist():
+#     file_zip.extract(file_txt)
+
+file_name = 'voyna-i-mir.txt'
+
+stat = {}
+
+with open(file=file_name, mode='r', encoding='cp1251') as file:
+    for line in file:
+        for char in line:
+            if char.isalpha():
+                if char in stat:
+                    stat[char] += 1
+                else:
+                    stat[char] = 1
+            else:
+                pass
+
+
+letter = 'Буква'
+hertz = 'Частота'
+plus = '+'
+pipe = '|'
+total = 'Итого'
+print(f'{plus}{plus:-^28}{plus}')
+print(f'{pipe}{letter:^12}{pipe:^4}{hertz:^12}{pipe}')
+print(f'{plus}{plus:-^28}{plus}')
+sorted_hertz = sorted(stat.items(), key=lambda x: x[1], reverse=True)
+for char, count in sorted_hertz:
+    print(f'{pipe}{char:^12}{pipe:^4}{count:^12}{pipe}')
+print(f'{plus}{plus:-^28}{plus}')
+print(f'{pipe}{total:^12}{pipe:^4}{sum(stat.values()):^12}{pipe}')
+print(f'{plus}{plus:-^28}{plus}')
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
