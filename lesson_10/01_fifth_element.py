5800732712ce14a7cfe48631c0caf7dad6a59d06
kunai <neni23@mail.ru>
2024-04-23 14:55:44 +0300

    01_fifth_element done!

--- lesson_10/01_fifth_element.py
+++ lesson_10/01_fifth_element.py
@@ -4,10 +4,17 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f"- Leeloo Dallas! Multi-pass № {result}!")
+except ValueError as exc:
+    print(f'Невозможно преобразовать к числу {exc}, параметры {exc.args}')
+except IndexError as exc:
+    print(f'Недостаточно символов/выход за границы списка {exc}, параметры {exc.args}')
+except Exception as exc:
+    print(f'Неизвестная ошибка - {exc}, параметры {exc.args}')
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
