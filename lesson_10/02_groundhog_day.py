91cf7a5cb63edfe5fe8ed3ce7d142cf06f7992cd
kunai <neni23@mail.ru>
2024-04-23 16:45:53 +0300

    02_groundhog started

--- lesson_10/02_groundhog_day.py
+++ lesson_10/02_groundhog_day.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 
 # День сурка
 #
@@ -17,8 +18,54 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 
-ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+class IamGodError(Exception):
+    pass
+
+class DrunkError(Exception):
+    pass
+
+
+class CarCrashError(Exception):
+    pass
+
+
+class GluttonyError(Exception):
+    pass
+
+
+class DepressionError(Exception):
+    pass
+
+
+class SuicideError(Exception):
+    pass
+
+
+ENLIGHTENMENT_CARMA_LEVEL = 777
+carma_lvl = 0
+exception = [IamGodError('Я сегодня БОГ'), DrunkError('Я сегодня НАПИЛСЯ'),
+             CarCrashError('Я сегодня разбился на ТАЧКЕ'), GluttonyError('Я сегодня ОБОЖРАЛСЯ'),
+             DepressionError('Я сегодня в ДЕПРЕССИИ'), SuicideError('Я сегодня СУИСАЙД')]
+
+
+def one_day():
+    res = random.randint(1, 7)
+    dice = random.random()
+    error = None
+    if round(dice, 2) <= round(1/13, 2):
+        error = random.choices(exception, k=1)
+        # try:
+        #     raise random.choices(exception, k=1)[0]
+        # except random.choices(exception, k=1)[0] as exc:
+        #     print(f'Поймали исключение {exc}')
+    return res, error
+
+
+while carma_lvl < ENLIGHTENMENT_CARMA_LEVEL:
+    carma_lvl += one_day()[0]
+    print(carma_lvl, one_day()[1])
+
+# print(random.choices(exception, k=1)[0])
 
 # https://goo.gl/JnsDqu
