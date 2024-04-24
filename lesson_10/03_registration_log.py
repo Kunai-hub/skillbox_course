86e743ed375d88a562e27cb94f3ad5488c53eab1
kunai <neni23@mail.ru>
2024-04-24 11:14:02 +0300

    03_registration_log started

--- lesson_10/03_registration_log.py
+++ lesson_10/03_registration_log.py
@@ -22,4 +22,45 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+class NotNameError(Exception):
+    pass
+
+
+class NotEmailError(Exception):
+    pass
+
+
+with open('registrations.txt', 'r', encoding='utf8') as file:
+    for line in file:
+        line_split = line.split()
+        try:
+            if len(line_split) == 3:
+                print(f'Строка соответствует требованиям: {line_split}')
+            else:
+                raise ValueError(f'Строка не соответствует требованиям: {line_split}')
+
+            if line_split[0].isalpha():
+                print(f'Имя состоит только из букв: {line_split[0]}')
+            else:
+                # print(f'Имя содержит не только буквы: {line_split[0]}')
+                raise NotNameError(f'Имя содержит не только буквы: {line_split[0]}')
+
+            if '@' and '.' in line_split[1]:
+                print(f'Емейл удовлетворяет требованиям: {line_split[1]}')
+            else:
+                # print(f'Емейл не содержит один из символов: "@", ".": {line_split[1]}')
+                raise NotEmailError(f'Емейл не содержит один из символов: "@", ".": {line_split[1]}')
+
+            if 10 <= int(line_split[2]) <= 99:
+                print(f'Возраст в пределах 10-99: {line_split[2]}')
+            else:
+                raise ValueError(f'Возраст не в пределах 10-99: {line_split[2]}')
+
+        except NotNameError as exc:
+            print(f'Ошибка имени: {exc}')
+        except NotEmailError as exc:
+            print(f'Ошибка емайла: {exc}')
+        except ValueError as exc:
+            print(f'Ошибка длины строки: {exc}')
+        except ValueError(f'Возраст не в пределах 10-99: {line_split[2]}') as exc:
+            print(f'Ошибка возраста: {exc}')
